3f5787a0b31e1f798354c0002e026a490e10a531
Daniil Danilov <k-ddanilo@c2r5p10.hive.fi>
2026-01-31 21:56:31 +0200

    Try to fix it

--- main.go
+++ main.go
@@ -62,6 +62,60 @@ func RemoveNote(fileName string, removeIndex int) {
     os.WriteFile(fileName, []byte(update), 0755)
 }
 
+func input() string {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+
+	return strings.TrimSpace(scanner.Text())
+
+}
+
+func menu() {
+
+	msg := ""
+
+	for msg != "4" {
+
+		fmt.Println("\n===Welcome to the notes tool!===\n")
+		fmt.Println("Select operation:")
+		fmt.Println("1. Show notes.")
+		fmt.Println("2. Add a note.")
+		fmt.Println("3. Delete a note.")
+		fmt.Println("4. Exit.")
+
+		msg = input()
+
+		if msg == "1" {
+
+			show()
+
+		} else if msg == "2" {
+			note := input()
+			AddNote(name, note)
+
+		} else if msg == "3" {
+			index := input()
+			RemoveNote(name ,index)
+
+		} else if msg == "4" {
+
+			fmt.Println("===Goodbye!===\n")
+
+			os.Exit(0)
+
+		}  else {
+
+			fmt.Println("\n===You have entered an invalid input!===\n")
+			continue
+
+		}
+
+
+	}
+}
+
 func main() {
-	AddNote("sello", "hello world")
+	 menu()
 }
