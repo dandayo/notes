cbd1428d6495665ba49bce3a2edf8c34890465af
Daniil Danilov <k-ddanilo@c2r5p10.hive.fi>
2026-01-31 18:49:51 +0200

    Try to figure out with remove element from file

--- newFile.go
+++ newFile.go
@@ -3,40 +3,61 @@ package main
 import (
     "fmt"
     "os"
+    "bufio"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func CreateFile() {
     file, err := os.Create("new.txt")
-    if err != nil {
-      fmt.Println("Mistake, try again")
-    }
+    check(err)
     defer file.Close()
     fmt.Println("File created successfully")
 }
 
 func DeleteFile() {
     err := os.Remove("new.txt")
-    if err != nil {
-        fmt.Println(err)
-    }
+    check(err)
     fmt.Println("File deleted")
 }
 
 func WriteInFile() {
     file, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-    if err != nil {
-    	fmt.Println("Mistake, try again")
-    }
+    check(err)
     defer file.Close()
 
     data := "new new info \n"
     _, err = file.WriteString(data)
-    if err != nil {
-    	fmt.Println("Mistake, try again")
-    }
+    check(err)
 
 }
 
+func ReadFile() {
+	notes, err := os.Open("new.txt")  //open the file
+	check(err)
+    defer notes.Close()
+
+    scanner := bufio.NewScanner(notes)  //scan the contents of a file and print line by line
+    for scanner.Scan() {
+        line := scanner.Text()
+        fmt.Println(line)
+    }
+    check(err)
+}
+
+
+func RemoveElement(array []string, n int)[]string{
+		if (len (array)< 1|| n-1 > len (array)){
+			return array
+		}
+		return append (array[:n], array[n+1:]...)
+}
+
 func main() {
-	WriteInFile()
+	read := ReadFile()
+	return read[0]
 }
