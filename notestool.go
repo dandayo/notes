7d67e38e4cbba3102922cad541a6ceb495f4c965
Ryan Rodrigo Durante <k-rdurant@c2r1p2.hive.fi>
2026-01-31 14:14:50 +0200

    added menu() and input()

---
+++ notestool.go
@@ -0,0 +1,89 @@
+package main
+import ("bufio"
+		"fmt"
+		"os"
+	//	"strconv"
+	//	"strings"
+		)
+
+
+func input() string {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()  
+
+
+	return scanner.Text()
+
+}
+
+
+func show() {
+
+
+}
+
+
+func add() {
+
+
+}
+
+
+func delete() {
+
+
+}
+
+func menu() {
+
+	msg := ""
+
+	for msg != "4" {
+
+		fmt.Println("\n===Welcome to the notes tool!===\n")
+		fmt.Println("Select operation:")
+		fmt.Println("1. Show notes.")
+		fmt.Println("2. Add a note.")
+		fmt.Println("3. Delete a note.")
+		fmt.Println("4. Exit.")
+
+		msg = input()
+
+		if msg == "1" {
+
+			show()
+
+		} else if msg == "2" {
+
+			add()
+
+		} else if msg == "3" {
+
+			delete()
+
+		} else if msg == "4" {
+
+			fmt.Println("===Goodbye!===\n")
+
+			os.Exit(0)
+
+		}  else {
+
+			fmt.Println("\n===You have entered an invalid input!===\n")
+
+			continue
+
+		}
+
+
+	}
+
+
+}		
+
+
+func main() {
+
+	menu()
+}
