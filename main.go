9e8a282cb78e766dc9d09ac83fb2243dd65c9e32
daniildanilov <daniildanilov@noreply.localhost>
2026-02-02 07:49:01 +0000

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,16 @@ import (
 func main() {
 	EmptyTerminal()
 	fmt.Println("\033[32m\n===Welcome to the notes tool!===\033[0m")
-	fileName := os.Args[1]
-	if fileName == "" {
-		 fmt.Println("Usage: ./todotool [TAG]\nAlso use --help to read about this tool")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./todotool [TAG]\nAlso use --help to read about this tool")
+		return
 	}
-	OpenFile(fileName)
-	Menu(fileName)
+	var fileName string
+	if os.Args[1] == "--help"{
+		Help()
+	} else {
+		fileName = os.Args[1]
+		OpenFile(fileName)
+		Menu(fileName)
+	}
 }
