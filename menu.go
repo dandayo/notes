d4ffa5b7664a92e39ebe62fd748e477d0f248503
ryanrodrigodurante <ryanrodrigodurante@noreply.localhost>
2026-02-02 11:19:41 +0000

    Update menu.go

--- menu.go
+++ menu.go
@@ -7,7 +7,7 @@ import (
 //Menu func
 func Menu(name string) {
 	msg := ""
-	for msg != "4" {
+	for msg {
 		//Print for choose
 		fmt.Println("\nSelect operation:\n")
 		fmt.Println("\033[36m1. Show notes.\033[0m")
