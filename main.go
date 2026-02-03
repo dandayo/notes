9763e69ae6b6e0640910cca32c160f740f554d0d
daniildanilov <daniildanilov@noreply.localhost>
2026-02-03 13:40:21 +0000

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	EmptyTerminal() //Clean terminal
 	fmt.Println("\033[32m\n===Welcome to the notes tool!===\033[0m")
-	if len(os.Args) < 2 { //Check is it empty input
+	if len(os.Args) != 2 { //Check is it empty input
 		fmt.Println("Usage: ./todotool [TAG]\nAlso use --help to read about this tool")
 		return
 	}
