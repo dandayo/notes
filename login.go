a450f2e688f5de1b8a63464d926a8c6fe628bb41
dandayou <danil99danilov@gmail.com>
2026-03-10 19:44:01 +0200

    create a secret input func

--- login.go
+++ login.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -19,35 +15,30 @@ func CheckFolder() bool { //check is it first usage or not
 	return false
 }
 
-func CreatePassword() { //check do we have any notes or we need to create a password for notes
+func secretInput() { //input without showing the input in the terminal
+
+}
+
+func CreatePassword() string { //check do we have any notes or we need to create a password for notes
 	if !CheckFolder() {
 		err := os.Mkdir("secret", 0755) //create a folder to save the password
 		check(err)
 
-		f, err := os.Create("secret/" + "check" + ".txt")
+		file, err := os.Create("secret/" + "check" + ".txt")
 		check(err)
 
 		fmt.Println("Create a password for your notes, here is a hidden input, be careful")
 		password := input()
 
-		block, err := aes.NewCipher(key)
-		check(err)
-
-		aesgcm, err := cipher.NewGCM(block)
-		check(err)
-
-		nonce := make([]byte, aesgcm.NonceSize())
-
-		if _, err := io.ReadFull(rand.Reader, nonce){
-			check(err)
-		}
-
+		return password
 	} else {
-		CheckPassword()
+		password := input()
+		CheckPassword(password)
+		return password
 	}
 }
 
-func CheckPassword() bool { //check if the password is correct or not
+func CheckPassword(string) bool { //check if the password is correct or not
 
 	return false
 }
